5a8331e0cb28a4d67bb06f669fcfa968498d0aa9
Rimberse <kerimpltm@gmail.com>
2024-02-29 18:35:24 +0100

    Create POJO class for Payments table

---
+++ src/main/java/fr/efrei/ordersharingsystem/domain/Payment.java
@@ -0,0 +1,30 @@
+package fr.efrei.ordersharingsystem.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Check;
+
+@Entity
+@Table(name = "Payments")
+@Getter
+@Setter
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "userId", referencedColumnName = "id")
+    private User user;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "orderId", referencedColumnName = "id")
+    private Order order;
+
+    @Column(name = "amount", precision = 10, scale = 2, nullable = false)
+    private double amount;
+
+    @Column(name = "status", length = 20, nullable = false)
+    private Status status = Status.PENDING;
+}
