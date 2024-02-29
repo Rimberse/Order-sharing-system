43d0a5086dac358855fd92f1f4e242e1d60872f8
Rimberse <kerimpltm@gmail.com>
2024-02-29 18:46:18 +0100

    Create POJO class for Notifications table

---
+++ src/main/java/fr/efrei/ordersharingsystem/domain/Notifications.java
@@ -0,0 +1,27 @@
+package fr.efrei.ordersharingsystem.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "Notifications")
+@Getter
+@Setter
+public class Notifications {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "userId", referencedColumnName = "id")
+    private User user;
+
+    @Column(name = "message", nullable = false)
+    private String message;
+
+    @Column(nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Timestamp createdAt;
+}
