fb13485d6529a3c3f1f38110c282fb1222018698
Senhao JIANG <ben659@hotmail.com>
2024-03-10 21:45:40 +0100

    Notification aggregate

---
+++ src/main/java/fr/efrei/ordersharingsystem/aggregate/handlers/NotificationAggregateHandler.java
@@ -0,0 +1,36 @@
+package fr.efrei.ordersharingsystem.aggregate.handlers;
+
+import fr.efrei.ordersharingsystem.aggregate.NotificationAggregateService;
+import fr.efrei.ordersharingsystem.aggregate.ProductAggregateService;
+import fr.efrei.ordersharingsystem.commands.notifications.SendNotificationCommand;
+import fr.efrei.ordersharingsystem.commands.products.CreateProductCommand;
+import fr.efrei.ordersharingsystem.commands.products.DeleteProductCommand;
+import fr.efrei.ordersharingsystem.commands.products.ModifyProductCommand;
+import fr.efrei.ordersharingsystem.domain.Notifications;
+import fr.efrei.ordersharingsystem.domain.Product;
+import fr.efrei.ordersharingsystem.domain.User;
+import fr.efrei.ordersharingsystem.exceptions.ItemNotFoundException;
+import fr.efrei.ordersharingsystem.repositories.NotificationRepository;
+import fr.efrei.ordersharingsystem.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class NotificationAggregateHandler implements NotificationAggregateService {
+
+    @Autowired
+    private final NotificationRepository notificationRepository;
+
+    public void handle(SendNotificationCommand command) {
+        var users = command.users();
+        for (User user: users
+             ) {
+            var notifications = new Notifications();
+            notifications.setUser(user);
+            notifications.setMessage(command.message());
+            notificationRepository.save(notifications);
+        }
+    }
+}
---
+++ src/main/java/fr/efrei/ordersharingsystem/commands/notifications/SendNotificationCommand.java
@@ -0,0 +1,14 @@
+package fr.efrei.ordersharingsystem.commands.notifications;
+
+import fr.efrei.ordersharingsystem.domain.User;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+public record SendNotificationCommand(
+        @NotBlank List<User> users,
+        @NotBlank String message
+) {
+}
