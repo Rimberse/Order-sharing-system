bb99d2425825fd47c256c4323c04072fe667838d
afkbeto <qv.nguyen00@gmail.com>
2024-03-10 11:44:28 +0100

    add: split sql queries and add enum

---
+++ db/drop.sql
@@ -0,0 +1,10 @@
+DROP TABLE notifications;
+DROP TABLE payments;
+DROP TABLE order_items;
+DROP TABLE orders;
+DROP TABLE products;
+DROP TABLE users;
+DROP TABLE alleys;
+DROP TABLE bowling_parks;
+DROP TYPE status;
+DROP TYPE role;
--- db/script.sql
+++ db/script.sql
@@ -1,157 +1,143 @@
-CREATE TABLE BowlingParks (
+CREATE TABLE bowling_parks (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     location VARCHAR(255) NOT NULL
 );
 
-INSERT INTO BowlingParks (name, location) VALUES (
+INSERT INTO bowling_parks (name, location) VALUES (
 	'Paris Stalingrad park', 
 	'5-1 Pl. de la Bataille de Stalingrad, 75010 Paris'
 );
 
-INSERT INTO BowlingParks (name, location) VALUES (
+INSERT INTO bowling_parks (name, location) VALUES (
 	'Paris République park', 
 	'5 Avenue de la République, 75011 Paris'
 );
 
-CREATE TABLE Alleys (
-    number INTEGER CONSTRAINT ck_number_in_range CHECK (number >= 1 AND number <= 20),
-    parkId INTEGER REFERENCES BowlingParks(id),
-    PRIMARY KEY (number, parkId)
+CREATE TABLE alleys (
+    number INT CONSTRAINT ck_number_in_range CHECK (number >= 1 AND number <= 20),
+    park_id INTEGER REFERENCES bowling_parks (id),
+    PRIMARY KEY (number, park_id)
 );
 	
 DO $$ 
 BEGIN 
     FOR i IN 1..20 LOOP
-        INSERT INTO Alleys (number, parkId) VALUES (i, 1);
-		INSERT INTO Alleys (number, parkId) VALUES (i, 2);
+        INSERT INTO alleys (number, park_id) VALUES (i, 1);
+        INSERT INTO alleys (number, park_id) VALUES (i, 2);
     END LOOP;
 END $$;
 
-CREATE TABLE Users (
+CREATE TYPE role AS ENUM('CUSTOMER', 'AGENT');
+
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    firstName VARCHAR(50),
-    lastName VARCHAR(50),
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
     name VARCHAR(50) UNIQUE,
     email VARCHAR(100) UNIQUE,
     password VARCHAR(255) NOT NULL,
-    phoneNumber VARCHAR(20),
-    role VARCHAR(10) NOT NULL,
-    assignedBowlingParkId INTEGER REFERENCES BowlingParks(id)
+    phone_number VARCHAR(20),
+    role ROLE NOT NULL,
+    assigned_bowling_park_id INTEGER REFERENCES bowling_parks (id)
 );
 
-INSERT INTO Users (firstName, lastName, name, email, password, phoneNumber, role) VALUES (
-	'John', 'Doe', 'johnDoe', 'john.doe@example.com', '123456789', '06 01 02 03 04', 'customer'
+INSERT INTO users (first_name, last_name, name, email, password, phone_number, role) VALUES (
+	'John', 'Doe', 'johnDoe', 'john.doe@example.com', '123456789', '06 01 02 03 04', 'CUSTOMER'
 );
 
-INSERT INTO Users (name, email, password, role, assignedBowlingParkId) VALUES (
-	'catalogManager1', 'catalog.manager1.paris-stalingrad@bowlingpark.fr', '123456789', 'agent', 1
+INSERT INTO users (name, email, password, role, assigned_bowling_park_id) VALUES (
+	'catalogManager1', 'catalog.manager1.paris-stalingrad@bowlingpark.fr', '123456789', 'AGENT', 1
 );
 
-INSERT INTO Users (name, email, password, role, assignedBowlingParkId) VALUES (
-	'orderManager1', 'order.manager1.paris-stalingrad@bowlingpark.fr', '123456789', 'agent', 1
+INSERT INTO users (name, email, password, role, assigned_bowling_park_id) VALUES (
+	'orderManager1', 'order.manager1.paris-stalingrad@bowlingpark.fr', '123456789', 'AGENT', 1
 );
 	
-CREATE TABLE Products (
+CREATE TABLE products (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     description VARCHAR(255),
     price INTEGER NOT NULL
 );
 
-INSERT INTO Products (name, description, price) VALUES (
+INSERT INTO products (name, description, price) VALUES (
 	'Bowling pin', 'Bowling pin. Used to play bowling', 1250
 );
 
-INSERT INTO Products (name, description, price) VALUES (
+INSERT INTO products (name, description, price) VALUES (
 	'Bowling ball', 'Bowling ball. Used to play bowling', 2750
 );
 
-INSERT INTO Products (name, price) VALUES (
+INSERT INTO products (name, price) VALUES (
 	'Beer', 399
 );
 
-INSERT INTO Products (name, price) VALUES (
+INSERT INTO products (name, price) VALUES (
 	'T-shirt', 200
 );
 
-CREATE TABLE Orders (
+CREATE TYPE status AS ENUM('PENDING', 'IN_PROGRESS', 'FAILED', 'COMPLETED', 'CANCELLED');
+
+CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
-    userId INTEGER REFERENCES Users(id),
-    parkId INTEGER REFERENCES BowlingParks(id),
-    alleyNumber INTEGER NOT NULL,
-    FOREIGN KEY (alleyNumber, parkId) REFERENCES Alleys (number, parkId),
-    status VARCHAR(20) DEFAULT 'PENDING'
+    user_id INTEGER REFERENCES users(id),
+    park_id INTEGER REFERENCES bowling_parks (id),
+    alley_number INTEGER NOT NULL,
+    FOREIGN KEY (alley_number, park_id) REFERENCES alleys (number, park_id),
+    status STATUS DEFAULT 'PENDING'
 );
 
-INSERT INTO Orders (userId, parkId, alleyNumber) VALUES (
+INSERT INTO orders (user_id, park_id, alley_number) VALUES (
 	1, 1, 3
 );
 
-INSERT INTO Orders (userId, parkId, alleyNumber) VALUES (
+INSERT INTO orders (user_id, park_id, alley_number) VALUES (
 	1, 1, 3
 );
 
-CREATE TABLE OrderItems (
+CREATE TABLE order_items (
 	id SERIAL PRIMARY KEY,
-	orderId INTEGER REFERENCES Orders(id),
-	productId INTEGER REFERENCES Products(id),
+	order_id INTEGER REFERENCES orders(id),
+	product_id INTEGER REFERENCES products(id),
 	quantity INTEGER NOT NULL
 );
 
-INSERT INTO OrderItems (orderId, productId, quantity) VALUES (
+INSERT INTO order_items (order_id, product_id, quantity) VALUES (
 	1, 1, 3
 );
 
-INSERT INTO OrderItems (orderId, productId, quantity) VALUES (
+INSERT INTO order_items (order_id, product_id, quantity) VALUES (
 	2, 2, 7
 );
 
-CREATE TABLE Payments (
+CREATE TABLE payments (
     id SERIAL PRIMARY KEY,
-    userId INTEGER REFERENCES Users(id),
-    orderId INTEGER REFERENCES Orders(id),
+    user_id INTEGER REFERENCES users(id),
+    order_id INTEGER REFERENCES orders(id),
     amount INTEGER NOT NULL,
-    status VARCHAR(20) DEFAULT 'PENDING'
+    status STATUS DEFAULT 'PENDING'
 );
 
-INSERT INTO Payments (userId, orderId, amount) VALUES (
+INSERT INTO payments (user_id, order_id, amount) VALUES (
 	1, 1, 8750
 );
 
-INSERT INTO Payments (userId, orderId, amount) VALUES (
+INSERT INTO payments (user_id, order_id, amount) VALUES (
 	1, 2, 2750
 );
 
-CREATE TABLE Notifications (
+CREATE TABLE notifications (
     id SERIAL PRIMARY KEY,
-    userId INTEGER REFERENCES Users(id),
+    user_id INTEGER REFERENCES users(id),
     message VARCHAR(255) NOT NULL,
-    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO Notifications (userId, message) VALUES (
+INSERT INTO notifications (user_id, message) VALUES (
 	1, 'thank you for joining!'
 );
 
-INSERT INTO Notifications (userId, message) VALUES (
+INSERT INTO notifications (user_id, message) VALUES (
 	2, 'thank you for singing a contract with us!'
 );
-
-SELECT * FROM BowlingParks;
-SELECT * FROM Alleys;
-SELECT * FROM Users;
-SELECT * FROM Products;
-SELECT * FROM Orders;
-SELECT * FROM OrderItems;
-SELECT * FROM Payments;
-SELECT * FROM Notifications;
-
-DROP TABLE Notifications;
-DROP TABLE Payments;
-DROP TABLE OrderItems;
-DROP TABLE Orders;
-DROP TABLE Products;
-DROP TABLE Users;
-DROP TABLE Alleys;
-DROP TABLE BowlingParks;
