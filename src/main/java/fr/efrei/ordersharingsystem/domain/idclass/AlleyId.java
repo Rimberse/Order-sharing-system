c392c441452645193c2fb9ee344c91ff8fd747e7
afkbeto <qv.nguyen00@gmail.com>
2024-03-10 20:37:48 +0100

    fix: some small fixes

--- src/main/java/fr/efrei/ordersharingsystem/domain/idclass/AlleyId.java
+++ src/main/java/fr/efrei/ordersharingsystem/domain/idclass/AlleyId.java
@@ -2,11 +2,13 @@ package fr.efrei.ordersharingsystem.domain.idclass;
 
 import fr.efrei.ordersharingsystem.domain.BowlingPark;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
+@RequiredArgsConstructor
 public class AlleyId implements Serializable {
-    private Integer number;
-    private BowlingPark park;
+    private final Integer number;
+    private final BowlingPark park;
 }
