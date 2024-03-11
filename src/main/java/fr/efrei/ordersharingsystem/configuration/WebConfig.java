ac5ea5c8940c8adde85855499f9209b3757b6ae1
afkbeto <qv.nguyen00@gmail.com>
2024-03-11 11:45:12 +0100

    add: authorization interceptor

--- src/main/java/fr/efrei/ordersharingsystem/configuration/WebConfig.java
+++ src/main/java/fr/efrei/ordersharingsystem/configuration/WebConfig.java
@@ -1,5 +1,7 @@
 package fr.efrei.ordersharingsystem.configuration;
 
+import fr.efrei.ordersharingsystem.interceptors.AuthorizationInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -12,5 +14,18 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
+    }
+
+    @Bean
+    public AuthorizationInterceptor authorizationInterceptor() {
+        return new AuthorizationInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(org.springframework.web.servlet.config.annotation.InterceptorRegistry registry) {
+        registry
+                .addInterceptor(authorizationInterceptor())
+                .addPathPatterns("/api/v1/parks/**/products**")
+                ;
     }
 }
--- src/main/java/fr/efrei/ordersharingsystem/interceptors/GlobalExceptionHandler.java
+++ src/main/java/fr/efrei/ordersharingsystem/interceptors/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
-package fr.efrei.ordersharingsystem.controller;
+package fr.efrei.ordersharingsystem.interceptors;
 
 import fr.efrei.ordersharingsystem.exceptions.ItemNotFoundException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
